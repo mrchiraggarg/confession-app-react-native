78c82e02c9ae14ebf46aad8c5d16401a34fe1364
mrchiraggarg <gargchirag728@gmail.com>
2025-06-12 11:48:23 +0530

    fixed firebaseconfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, serverTimestamp } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFwFpJ3gruOUlY4CNemmpvWCatM1g9KPE",
-  authDomain: "confession-app-react-native.firebaseapp.com",
-  projectId: "confession-app-react-native",
-  storageBucket: "confession-app-react-native.firebasestorage.app",
-  messagingSenderId: "1048883653779",
-  appId: "1:1048883653779:web:d82bab08d2ba10c368b166",
-  measurementId: "G-4Q6M77R13B"
+  apiKey: "AIzaSyA2U743exwIVj37pMEMDMKpQA56WeEXvFw",
+  authDomain: "confession-app-f5a67.firebaseapp.com",
+  projectId: "confession-app-f5a67",
+  storageBucket: "confession-app-f5a67.firebasestorage.app",
+  messagingSenderId: "339307365760",
+  appId: "1:339307365760:web:a64bfcdd686e68bcb0ed65",
+  measurementId: "G-VP5F0BE7NW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, serverTimestamp };
